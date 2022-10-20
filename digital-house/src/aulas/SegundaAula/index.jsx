00ebf3e7483c60d43cdd9c7aed9d5d1e0bb8f3c3
danielspiker <danielmnascimento@gmail.com>
2022-10-20 18:28:35 -0300

    segunda aula react

---
+++ digital-house/src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,40 @@
+import './style.scss'
+
+export function SegundaAula() {
+  return (
+    <main>
+      <h1>Segunda Aula</h1>
+      <h2>Site escolhido: Youtube</h2>
+      <ul>
+        <li>
+          <h3>Primeiro componente</h3>
+          <p>Header</p>
+        </li>
+        <li>
+          <h3>Segundo componente</h3>
+          <p>Ícone de Menu</p>
+        </li>
+        <li>
+          <h3>Terceiro componente</h3>
+          <p>Logo</p>
+        </li>
+        <li>
+          <h3>Quarto componente</h3>
+          <p>Input de Pesquisa</p>
+        </li>
+        <li>
+          <h3>Quinto componente</h3>
+          <p>Ícone de Teclado</p>
+        </li>
+        <li>
+          <h3>Sexto componente</h3>
+          <p>Botão de Pesquisa</p>
+        </li>
+        <li>
+          <h3>Sétimo componente</h3>
+          <p>Ícone de Microfone</p>
+        </li>
+      </ul>
+    </main>
+  )
+}
---
+++ digital-house/src/pages/Login/index.jsx
@@ -0,0 +1,3 @@
+export function Login() {
+  return <h1>Eu sou um componente de Login</h1>
+}
