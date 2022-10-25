ad904d63b0ce9829d6229e113e875f377b082309
danielspiker <danielmnascimento@gmail.com>
2022-10-24 22:52:57 -0300

    quarta aula react

--- digital-house/src/App.jsx
+++ digital-house/src/App.jsx
@@ -1,13 +1,15 @@
 import { Login } from './pages/Login'
 import { SegundaAula } from './aulas/SegundaAula'
 import { TerceiraAula } from './aulas/TerceiraAula'
+import { QuartaAula } from './aulas/QuartaAula'
 
 function App() {
   return (
     <>
       {/* <Login /> */}
       {/* <SegundaAula /> */}
-      <TerceiraAula />
+      {/* <TerceiraAula /> */}
+      <QuartaAula />
     </>
   )
 }
